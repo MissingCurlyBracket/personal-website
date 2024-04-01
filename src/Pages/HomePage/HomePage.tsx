b2d65ad4b76fc7d118504a8fadb95ff570d81881
Lucian Negru <L.Negru@student.tudelft.nl>
2024-04-02 01:33:50 +0200

    Add basic project template

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './HomePage.css';
+import Project from "../../Templates/Projects/Project";
 
 export default function HomePage() {
-  return (
-    <header>
-      Lucian's Website
-    </header>
-  );
+    return (
+        <div className="home-page">
+            <header>
+                Lucian's Website
+            </header>
+            <Project name={"This website"} date={new Date("2024-04-1")} description={"I made this website"}
+                     skills={["React", "TypeScript", "Atomic Design", "TDD"]}/>
+        </div>
+    );
 }
