d1f6b5905b0511d1d35bda1eada46d96c6c41f01
Lucian Negru <L.Negru@student.tudelft.nl>
2024-04-17 19:34:36 +0200

    Update images

--- src/Organisms/Header/Header.tsx
+++ src/Organisms/Header/Header.tsx
@@ -4,7 +4,7 @@ import HeaderOption from "../../Atoms/HeaderOption/HeaderOption";
 export default function Header() {
     return (
         <div className="header">
-            <img src="%PUBLIC_URL%/logo192.png" alt="logo" className="logo"/>
+            <img src="./logo192.png" alt="logo" className="logo"/>
             <nav>
                 <ul>
                     <HeaderOption href='/' text="Home"/>
