835948a734db8edfff6f5c912085f01edd830ecc
Lucian Negru <L.Negru@student.tudelft.nl>
2024-06-09 23:12:03 +0200

    Add css grid

--- src/Pages/ExperiencePage/ExperiencePage.tsx
+++ src/Pages/ExperiencePage/ExperiencePage.tsx
@@ -1,24 +1,19 @@
 import data from './data.json';
 import React from "react";
+import './ExperiencePage.css';
+import ExperienceItem from "./ExperienceItem";
 
 export default function ExperiencePage() {
     return (
         <div className="experience-page">
             <h2>Experience</h2>
-            {data.map((experience) => (
-                <div key={experience.company}>
-                    <h3>{experience.company}</h3>
-                    <p>{experience.position}</p>
-                    <p>{experience.period}</p>
-                    <p>{experience.description}</p>
-                    <p>Applied skills: {experience.skills.map((skill, index) => (
-                        <React.Fragment key={index}>
-                            {skill}
-                            {index !== experience.skills.length - 1 && ', '}
-                        </React.Fragment>
-                    ))}</p>
-                </div>
-            ))}
+            <div className="experiences">
+                {data.map((experience) => (
+                    <ExperienceItem company={experience.company} position={experience.position}
+                                    period={experience.period}
+                                    description={experience.description} appliedSkills={experience.skills}/>
+                ))}
+            </div>
         </div>
     );
 }
